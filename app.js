a154e7fb8babaa165bcfb84a440a432a2695635c
Tauseef Mushtaq <tauseef205005@gmail.com>
2025-07-08 15:43:53 +0500

    .

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user; // Make currentUser available in all templates
+    res.locals.currUser = req.user || null; // Make currentUser available in all templates
     next();
 });
 
